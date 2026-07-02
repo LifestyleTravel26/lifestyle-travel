23b094640ea106e03e7070d04158daf881e01640
Jimmy Leon <mimotrix26@gmail.com>
2026-07-02 13:20:22 +0100

    fix: remove AIChat import from layout

--- lifestyle-travel/app/layout.tsx
+++ lifestyle-travel/app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { LanguageProvider } from "./context/LanguageContext";
 import CookieBanner from "./components/CookieBanner";
-import AIChat from "./components/AIChat";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,7 +53,6 @@ export default function RootLayout({
         <LanguageProvider>
           {children}
           <CookieBanner />
-          <AIChat />
         </LanguageProvider>
       </body>
     </html>
